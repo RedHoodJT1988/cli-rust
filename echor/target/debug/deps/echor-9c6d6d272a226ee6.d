0259b680b43833d308d3573ca0de45c9aad7494a
Jonathan Reeves <jonathanedwardreeves1988@gmai.com>
2025-03-09 15:46:42 -0500

    initial commit for echor project

---
+++ echor/target/debug/deps/echor-9c6d6d272a226ee6.d
@@ -0,0 +1,5 @@
+/Users/redhood/Desktop/code-projects/rust_projects/cli-rust/echor/target/debug/deps/echor-9c6d6d272a226ee6: src/main.rs
+
+/Users/redhood/Desktop/code-projects/rust_projects/cli-rust/echor/target/debug/deps/echor-9c6d6d272a226ee6.d: src/main.rs
+
+src/main.rs:
---
+++ echor/target/debug/echor.d
@@ -0,0 +1,1 @@
+/Users/redhood/Desktop/code-projects/rust_projects/cli-rust/echor/target/debug/echor: /Users/redhood/Desktop/code-projects/rust_projects/cli-rust/echor/src/main.rs
